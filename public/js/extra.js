5a204c601cdd3de4c4c6329b6eebc91081f09d7e
manishdeadflow <manishdeadflow@gmail.com>
2021-09-22 11:47:39 +0530

    responsive

--- public/js/extra.js
+++ public/js/extra.js
@@ -1,12 +1,12 @@
-const append='>>>'
-const terminalInput=document.querySelector('.terminalInput')
-const terminal=document.querySelector('.terminal')
-const help=document.querySelector('.help')
-const home=document.querySelector('.home')
+const append = ">>>";
+const terminalInput = document.querySelector(".terminalInput");
+const terminal = document.querySelector(".terminal");
+const help = document.querySelector(".help");
+const home = document.querySelector(".home");
 
 let weatherData;
 
-const def=`<p class="default">This is created for your interection<br>
+const def = `<p class="default">This is created for your interection<br>
 with weather apicommands aren't that hard <br>
 <br>
 type your command then press enter<br>
@@ -22,68 +22,81 @@ and you have to start everything from the start.<br>
 <br>
 -Enter help for more<br>
 -Enter exit for exiting <br>
-  </p>`
+  </p>`;
 
-terminal.insertAdjacentHTML('beforeend',def)
+terminal.insertAdjacentHTML("beforeend", def);
 
-document.addEventListener('keydown',(e)=>{
-   if(e.key==='Enter'){
-     const input=terminalInput.value;
-     terminalInput.value="";
-    
-     if(document.querySelector('.default'))
-       terminal.removeChild(document.querySelector('.default'))
-     terminal.insertAdjacentHTML('beforeend',`<p class='textInput'>>>>  ${input}</p>`)
-     
-     //help input ->redirect to help page
-     if(input==='help'||input==='h'){
-        help.click()
-     } 
-     //exit input -> redirect to home page
-     else if(input==='exit'||input==='q'){
-        home.click()
-     }
-     //clear the terminal
-     else if(input==='clear'){
-       let textInput=document.querySelectorAll('.textInput')
-       let textReturn=document.querySelectorAll('.textReturn')
-       let textError=document.querySelectorAll('.textError')
-       if(textInput)
-         textInput.forEach(element => {
-           element.parentNode.removeChild(element)
-         });
-       if(textError)
-        textError.forEach(element => {
-           element.parentNode.removeChild(element)
-         });
-       if(textReturn)
-         textReturn.forEach(element => {
-           element.parentNode.removeChild(element)
-         });
-       weatherData=undefined
-     }
-     else{
-        if(!weatherData){
-          fetch(`http://localhost:3000/weather2?address=${input}`).then((response)=>{
-            response.json().then((data)=>{
-              if(data.error){
-                terminal.insertAdjacentHTML('beforeend',`<p class='textError'>>>>  ${data.error}</p>`)
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const input = terminalInput.value;
+    terminalInput.value = "";
+
+    if (document.querySelector(".default"))
+      terminal.removeChild(document.querySelector(".default"));
+    terminal.insertAdjacentHTML(
+      "beforeend",
+      `<p class='textInput'>>>>  ${input}</p>`
+    );
+
+    //help input ->redirect to help page
+    if (input === "help" || input === "h") {
+      help.click();
+    }
+    //exit input -> redirect to home page
+    else if (input === "exit" || input === "q") {
+      home.click();
+    }
+    //clear the terminal
+    else if (input === "clear") {
+      let textInput = document.querySelectorAll(".textInput");
+      let textReturn = document.querySelectorAll(".textReturn");
+      let textError = document.querySelectorAll(".textError");
+      if (textInput)
+        textInput.forEach((element) => {
+          element.parentNode.removeChild(element);
+        });
+      if (textError)
+        textError.forEach((element) => {
+          element.parentNode.removeChild(element);
+        });
+      if (textReturn)
+        textReturn.forEach((element) => {
+          element.parentNode.removeChild(element);
+        });
+      weatherData = undefined;
+    } else {
+      if (!weatherData) {
+        fetch(`http://localhost:3000/weather2?address=${input}`).then(
+          (response) => {
+            response.json().then((data) => {
+              if (data.error) {
+                terminal.insertAdjacentHTML(
+                  "beforeend",
+                  `<p class='textError'>>>>  ${data.error}</p>`
+                );
+              } else {
+                weatherData = data;
+                terminal.insertAdjacentHTML(
+                  "beforeend",
+                  `<p class='textReturn'>>>>  weather data loaded ! you can start your queries now</p>`
+                );
               }
-              else{
-                weatherData=data
-                terminal.insertAdjacentHTML('beforeend',`<p class='textReturn'>>>>  weather data loaded ! you can start your queries now</p>`)
-              }
-            })
-          })
-        }
-        else{
-          if(weatherData.data[input]){
-            terminal.insertAdjacentHTML('beforeend',`<p class='textReturn'>>>>  ${weatherData.data[input]}</p>`)
+            });
           }
-          else{
-            terminal.insertAdjacentHTML('beforeend',`<p class='textError'>>>> no property named  ${input} !</p>`)
-          }
+        );
+      } else {
+        if (weatherData.data[input]) {
+          terminal.insertAdjacentHTML(
+            "beforeend",
+            `<p class='textReturn'>>>>  ${weatherData.data[input]}</p>`
+          );
+        } else {
+          terminal.insertAdjacentHTML(
+            "beforeend",
+            `<p class='textError'>>>> no property named  ${input} !</p>`
+          );
         }
-     }
-   }
-})
+      }
+    }
+  }
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,23 +1,23 @@
-const request=require('request')
+const request = require("request");
 
-const forecast=(latitude,longitude,callback)=>{
-   const key1='2514c9d7feee820b97246a73d7b32858'
-   const key2='dc0b4138293f9e51a5aa484c5794df5a'  
+const forecast = (latitude, longitude, callback) => {
+  const key1 = "2514c9d7feee820b97246a73d7b32858";
+  const key2 = "dc0b4138293f9e51a5aa484c5794df5a";
 
-     const url=`http://api.weatherstack.com/current?access_key=${key2}&query=${latitude} ${longitude}&units=m`
-        
-    request({url:url,json:true},(error,{body}={})=>{
-        if(error){
-            callback('unable to connect to weather service!',undefined)
-        }
-        else if(body.error){
-            callback('unable to find location',undefined)
-        }
-        else {
-            callback(undefined,{logo:body.current.weather_icons[0],
-              forecast:`${body.current.weather_descriptions[0]}.it is currently ${body.current.temperature} degrees.it feels like it is ${body.current.feelslike} degrees out`})
-        }
-    })
-}
+  const url = `http://api.weatherstack.com/current?access_key=${key2}&query=${latitude} ${longitude}&units=m`;
 
-module.exports=forecast
+  request({ url: url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("unable to find location", undefined);
+    } else {
+      callback(undefined, {
+        logo: body.current.weather_icons[0],
+        forecast: `${body.current.weather_descriptions[0]}.it is currently ${body.current.temperature} degrees.it feels like it is ${body.current.feelslike} degrees out`,
+      });
+    }
+  });
+};
+
+module.exports = forecast;
